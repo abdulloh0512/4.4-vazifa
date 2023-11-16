b31299a07574566b557160cf248f74a409b07636
abdulloh0512 <abdullohdev0512@gmail.com>
2023-11-16 20:26:51 +0500

    js 4.4 vazifa

---
+++ script.js
@@ -0,0 +1,128 @@
+// 1-masala
+//  let k = prompt("k sonning o'rniga son kiriting.")
+// let g = +prompt("n sonning o'rniga son kiriting.")
+// for (let i = 1; i <= g; i++) {
+//     console.log(k + "\n");
+// }
+
+// 2-masala
+// let a = prompt("a sonni kiriting.")
+// let b = prompt("b sonni kiriting.")
+// for (let i = a;  i <= b; i++) {
+//     console.log(i + "\n");
+// }
+
+// 3-masala
+// let a = prompt("a sonni kiriting.")
+// let b = prompt("b sonni kiriting.")
+// for (let i = --b;  i > a; i--) {
+//     console.log(i + "\n");
+// }
+
+// 4-masala
+// let k = +prompt("Konfetning narxini kiriting.");
+// let narx = "";
+// for (let i = 1; i <= 10; i++) {
+//   narx = narx + i + "kg konkfet " + i * k + " so'm " + "\n";
+// }
+// console.log(narx);
+
+// 5-masala
+// let k = +prompt("Konfetning narxini kiriting.");
+// let narx = "";
+// for (let i = 0.1; i <= 1; (i + 0.1)) {
+//   narx = narx + i + "kg konkfet " + i * k + " so'm " + "\n";
+// }
+// console.log(narx);
+
+// 6-masala
+// let k = +prompt("Konfetning narxini kiriting.");
+// let narx = "";
+// for (let i = 1.2; i <= 2; (i + 0.2)) {
+//   narx = narx + i + "kg konkfet " + i * k + " so'm " + "\n";
+// }
+// console.log(narx);
+
+// 7-masala
+// let a = +prompt ("a sonini kiriting");
+// let b = +prompt ("b sonini kiriting");
+// if (a < b) {
+//   let sum = 0;
+//   for (let i = a; i <= b; i++) {
+//     sum += i;
+//   }
+//   console.log (`${a} dan ${b} gacha bo'lgan sonlar yig'indisi ${sum} ga teng`);
+// } else {
+//   console.log ("a soni b sonidan kichik bo'lishi kerak");
+// }
+
+// 8-masala
+// let a = +prompt("a sonini kiriting");
+// let b = +prompt("b sonini kiriting");
+
+// if (a < b) {
+//   let kopaytma = 1;
+//   for (let i = a; i <= b; i++) {
+//     kopaytma *= i;
+//   }
+//   console.log(`${a} dan ${b} gacha bo'lgan sonlar kopaytmasi ${kopaytma} ga teng`);
+// } else {
+//   console.log("a soni b sonidan kichik bo'lishi kerak");
+// }
+
+// 9-masala
+// let a = +("a sonini kiriting");
+// let b = +("b sonini kiriting");
+
+// if (a < b) {
+
+//   let yigindi = 0;
+//   for (let i = a; i <= b; i++) {
+//     yigindi += (i * i);
+//   }
+//   console.log(`${a} dan ${b} gacha bo'lgan sonlar kvadratlarining yig'indisi ${yigindi} ga teng`);
+// } else {
+//   console.log("a soni b sonidan kichik bo'lishi kerak");
+// }
+
+// 10-masala
+// let n = +prompt("n sonini kiriting");
+
+// if (n > 0) {
+//   let yigindi = 0;
+//   for (let i = 1; i <= n; i++) {
+//     yigindi += 1 / i;
+//   }
+//   console.log(`1 + 1/2 + 1/3 + ... + 1/${n} yig'indisi ${yigindi} ga teng`);
+// } else {
+//   console.log("n soni musbat bo'lishi kerak");
+// }
+
+// 11-masala
+// let n = +prompt("n sonini kiriting");
+
+// if (n > 0) {
+//   let yigindi = 0;
+//   for (let i = n; i <= 2 * n; i++) {
+//     yigindi += i * i;
+//   }
+//   console.log(
+//     `S = ${n}^2 + (${n}+1)^2 + (${n}+2)^2 + ... + (2*${n})^2 yig'indisi ${yigindi} ga teng`
+//   );
+// } else {
+//   console.log("n soni musbat bo'lishi kerak");
+// }
+
+// 12-masala
+// let n = +prompt("n sonini kiriting");
+
+// if (n > 0) {
+//   let yigindi = 1.1;
+//   for (let i = 1.2; i <= n; i++) {
+//     yigindi *= i; 
+//   }
+//   console.log(`S = 1.1*1.2*1.3*...*${n} ko'paytmasi ${yigindi} ga teng`);
+// } else {
+//   console.log("n soni musbat bo'lishi kerak");
+// }
+
